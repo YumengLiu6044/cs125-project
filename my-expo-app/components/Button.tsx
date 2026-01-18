55fcfef763715d56acdc8c07885ed7b20a489b12
Yumeng Liu <lym20041026@gmail.com>
2026-01-18 13:46:25 -0800

    allow buttons to have an icon

--- my-expo-app/components/Button.tsx
+++ my-expo-app/components/Button.tsx
@@ -1,27 +1,31 @@
-import { Pressable, Text } from 'react-native';
+import { IconNode } from 'lucide-react-native';
+import { ReactNode } from 'react';
+import { Pressable, Text, View } from 'react-native';
 
 interface ButtonProps {
   onPress?: () => void;
   title: string;
   style?: keyof typeof ButtonStyles;
+  icon?: ReactNode | (() => ReactNode);
+  className?: string;
 }
 
 export default function Button({
   onPress,
   title,
   style = 'primary',
+  icon,
+  className,
 }: ButtonProps) {
   const selectedStyle = ButtonStyles[style];
 
   return (
     <Pressable
       onPress={onPress}
-      className={`flex w-full items-center justify-center rounded-2xl px-[24px] py-[16px] ${selectedStyle.pressable}`}
-      accessibilityLabel={title}
-    >
-      <Text className={`font-bold text-lg ${selectedStyle.text}`}>
-        {title}
-      </Text>
+      className={`flex-row items-center justify-center rounded-2xl px-[24px] py-[16px] ${selectedStyle.pressable} ${className}`}
+      accessibilityLabel={title}>
+      {icon && <View className='mr-2'>{typeof icon === 'function' ? icon() : icon}</View>}
+      <Text className={`font-bold text-lg ${selectedStyle.text}`}>{title}</Text>
     </Pressable>
   );
 }
