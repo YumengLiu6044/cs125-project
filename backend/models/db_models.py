f996dd140db6dce525aca2e7118f1e0fc2e9fc37
Yumeng Liu <lym20041026@gmail.com>
2026-01-29 09:30:51 -0800

    user beanie for ODM

--- backend/models/db_models.py
+++ backend/models/db_models.py
@@ -1,9 +1,9 @@
 from typing import Optional
 import pymongo
 from beanie import Document, Indexed
-from pydantic import EmailStr, Field
+from pydantic import EmailStr
 
 class Users(Document):
-    email: Indexed(EmailStr, unique=True, index_type=pymongo.TEXT)
+    email: Indexed(EmailStr, unique=True, index_type=pymongo.ASCENDING)
     username: Optional[str]
     password: Optional[str]
